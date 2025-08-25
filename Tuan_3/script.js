10139711d01edbc5dae45aede6f4b32680318c99
DevMikeJ28 <tungvsfot@gmail.com>
2025-08-26 00:45:41 +0700

    V_00.2: Update isModified function

--- Tuan_3/script.js
+++ Tuan_3/script.js
@@ -4,7 +4,7 @@
 const oldData = {
   type: "div",
   props: {
-    id: "container",
+    id: "app",
     children: [
       { type: "h1", props: { children: ["Hello World"] } },
       { type: "p", props: { children: ["This is a paragraph"] } },
@@ -15,54 +15,78 @@ const oldData = {
 const newData = {
   type: "div",
   props: {
-    id: "container",
+    id: "app",
     children: [
-      { type: "h1", props: { children: ["Hello React"] } }, // text thay đổi
-      { type: "p", props: { children: ["This is an updated paragraph"] } }, // text thay đổi
-      { type: "button", props: { children: ["Click Me"] } }, // thêm node mới
+      { type: "h1", props: { children: ["Hello React"] } },
+      { type: "p", props: { children: ["This is an updated paragraph"] } },
+      { type: "button", props: { children: ["Click Me"] } },
     ],
   },
 };
 
 /**
- * This function use to modify the child node, 
+ * This function use to modify the child node,
  * if that parent node does not has child, the old child will be null
- * 
+ *
  * @param { object } parent
  * @param { object } newNode
  * @param { object } oldNode
  */
 
-const updateElement = (parent, newNode, oldNode = null) => {};
+const updateElement = (parent, newNode, oldNode = null) => {
+  // First case: adding child or modifying the root node:
+  if (oldNode === null) {
+    // Modyfing the root node:
+    if (parent === null) {
+    }
+  }
+};
 
 /**
  * This function use to check the different between 2 VDOM
- * 
+ *
  * @param { object } oldNode
  * @param { object } newNode
  */
-const diffHeuristic = (oldNode, newNode) => {
-  let isNeedToUpdate = false;
+const diffHeuristic = (oldNode, newNode, parentNode = null) => {
+  console.log(isModified(oldNode, newNode));
 
-  //step 1: Compare type of node:
-  if (oldNode?.type !== newNode?.type) return true;
+  //Handle case that is the root node:
+  if (!parentNode && isModified(oldNode, newNode)) {
+    return newNode;
+  }
 
-
-  
-
-
-  return isNeedToUpdate
+  // Continue to the child:
 };
 
+/**
+ *
+ * @param {object} oldNode
+ * @param {object} newNode
+ * @returns
+ */
+const isModified = (oldNode, newNode) => {
+  //Step 1: Compare the type of the element:
+  if (newNode?.type !== oldNode?.type) return true;
 
-const compare = (newNode, oldNode) {
+  //Step 2: Compare the attribute:
+  const { childOld, restOld } = { ...oldNode };
+  const { childNew, restNew } = { ...newNode };
 
-  if(oldNode?.type !== newNode?.type)
+  if (typeof restOld !== typeof restNew) return true;
+  if (restNew && restNew && Object.keys(restNew) !== Object.keys(restOld))
+    return true;
 
-}
+  //Step 2: Compare the child
+  if (childOld?.length !== childNew?.length) return true;
+
+  return false;
+};
 
 const main = () => {
-  diffHeuristic(oldData, newData);
+  let VDOM = diffHeuristic(oldData, newData);
+  console.log(VDOM);
+  return VDOM;
 };
 
 main();
